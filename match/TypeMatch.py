d8f417bd1ae0936f1022b9759c250022e8c9d4f1
sz <shenz34206@hotmail.com>
2014-08-01 15:11:06 -0700

    Edit doc for TypeMatch

--- match/TypeMatch.py
+++ match/TypeMatch.py
@@ -1,6 +1,7 @@
 import pickle
 
-def TypeChain(inp,dic)->list:
+#def TypeChain(inp,dic) -> list:
+def TypeChain(inp,dic):
     if dic[inp]=="":
         ans=[inp]
     else:
@@ -8,17 +9,28 @@ def TypeChain(inp,dic)->list:
         ans.append(inp)
     return ans
 
-dicFile=open('dataDic.dat','rb')
-dataDic=pickle.load(dicFile)
-dicFile.close()
+def TypeMatch(a,b):
 
-a='String'
-b='Place'
-aC=TypeChain(a,dataDic)
-bC=TypeChain(b,dataDic)
-n=0
-minLen=min(len(aC),len(bC))
-while n+1<minLen and aC[n+1]==bC[n+1]:
-    n+=1
-print(n)
+    """
+    Compute how like two types is.
 
+    >>> TypeMatch('String','Place')
+    1
+    """
+
+    dicFile=open('dataDic.dat','rb')
+    dataDic=pickle.load(dicFile)
+    dicFile.close()
+
+    aC=TypeChain(a,dataDic)
+    bC=TypeChain(b,dataDic)
+    n=0
+    minLen=min(len(aC),len(bC))
+    while n+1<minLen and aC[n+1]==bC[n+1]:
+        n+=1
+    return n
+
+#print(TypeMatch('String','Time'))
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
