60a975b4a393e96c5b426fdf6ca6b552a3ead177
haleymuhl <haleymuhl@gmail.com>
2014-08-06 10:36:14 -0700

    renew

--- Haley/open_url.py
+++ Haley/open_url.py
@@ -1,21 +1,30 @@
 import urllib.request
 import csv
-import xlrd
-
-
-
 import os 
-import pandas
+import re
+
+national_debt = "http://api.worldbank.org/v2/en/indicator/gc.dod.totl.gd.zs?downloadformat=excel"
+co2 = "http://api.worldbank.org/v2/en/indicator/en.atm.co2e.pc?downloadformat=excel"
+agriculture_value_added = "http://api.worldbank.org/v2/en/indicator/nv.agr.totl.zs?downloadformat=excel"
+tuberculosis = "http://api.worldbank.org/v2/en/indicator/sh.tbs.incd?downloadformat=excel"
+GDP = "http://api.worldbank.org/v2/en/indicator/ny.gdp.mktp.cd?downloadformat=excel"
+
 
 os.chdir("C://Users//HaleyPC//Documents//HIKA//Haley")
 url = "http://api.worldbank.org/v2/en/indicator/ny.gdp.mktp.cd?downloadformat=excel"
 
 def get_data(url):
+	data_name = re.findall('/indicator/[a-z][a-z]\.[a-z][a-z][a-z]',url)[0][-3:]
 
 	testfile = urllib.request.URLopener()
-	testfile.retrieve(url, "test_file.xls")
+	testfile.retrieve(url, data_name + "_data.xls")
 
 	import pandas as pd
-	xls = pd.ExcelFile('test_file.xls')
+	xls = pd.ExcelFile(data_name + '_data.xls')
 	df = xls.parse(index_col=None, na_values=['NA'])
-	df.to_csv('test_file.csv')
+	df.to_csv(data_name + '_data.csv')
+	return(data_name + '_data.csv')
+
+
+x = get_data(agriculture_value_added)
+print(x)
