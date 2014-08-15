d4d09c608e97832c81080306494aa2e53c7492b4
qiao-jian <qiao.jian2014@yahoo.com>
2014-08-15 09:05:09 -0700

    Qiao

--- Qiao/data_crawler_loop.py
+++ Qiao/data_crawler_loop.py
@@ -1,9 +1,12 @@
 import urllib.request
+#Using BeautifulSoup Module
 from bs4 import BeautifulSoup
 import re
+#Loop the websites within same indicator
 for k in range(7):
     page=urllib.request.urlopen('http://data.worldbank.org/indicator/AG.LND.IRIG.AG.ZS?page='+str(k))
     soup=BeautifulSoup(page)
+#Show all the data pulled from website
     for tr in soup.find_all('tr')[1:]:
         tds = tr.find_all('td')
         for i in tds:
